649570654998d8d98640d29701bc59c2f220c218
root <root@lake-pc.(none)>
2012-04-08 02:12:31 +0800

    adjust some file positions

---
+++ lib/Loader.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * loader
+ */
+class DOLoader
+{
+	private static $loaded = array();
+	public static function AutoLoad($class)
+	{
+		include_once FRAMEWORK_ROOT.DS.''
+			    .'lib'.DS
+			    .str_replace('_',DS,preg_replace('#^DO#','',$class)).".php";
+	}
+	/**
+	 * multiple files load.
+	 *
+	 * @return true if loaded successfully,or false.
+	 */
+	function  Import()
+	{
+		$args  	= func_get_args();
+		if($args[0] == '') return;
+		foreach($args as $file)
+		{
+			if( !self::$loaded[ $file ] )
+			{
+				$f  = str_replace('.',DS,$file);
+				
+				$fs = FRAMEWORK_ROOT.DS.$f.'.php';
+
+				if( file_exists($fs) )
+				{
+					include $fs;
+				}
+				else 
+				{
+					return false;
+				}
+				self::$loaded[ $file ] = true;
+			}
+			else continue;
+		}
+		return true;
+	}
+	
+	
+}
+?>
---
+++ lib/Template.php
@@ -0,0 +1,50 @@
+<?php
+class DOTemplate
+{
+	public static $params = array();
+	public static function SetPrams($params)
+	{
+		foreach($params as $key=>$val) self::SetParam($key,$val);
+	}
+	
+	public static function SetParam($key,$val)
+	{
+		$key = strtolower($key);
+		echo $key;
+		self::$params[$key] = $val;
+	}
+	public static function LoadTemplate( $template )
+	{
+			ob_start();
+			include TEMPLATEROOT.DS.$template.DS.'index.php';
+			$content = ob_get_contents();
+			ob_end_clean();
+			return $content;
+	}
+	/**Core function,use to set hook all elements we want to display in template**/
+	public static function _()
+	{
+		$args 	= func_get_args();
+		$type	= strtolower(array_shift($args));
+		echo call_user_func_array(array(self,'GET'.ucwords($type)),$args);
+	}
+	public static function SetTitle( $title )
+	{
+		self::$params['title'] = $title;
+	}
+	public static function GetBlocks( $pos )
+	{
+		return self::$params['blocks'][$pos];
+	}
+	
+	public static function GetModule()
+	{
+		return self::$params['module'];
+	}
+	
+	public static function GetTitle()
+	{
+		return self::$params['title'];
+	}
+}
+?>
