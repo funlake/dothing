4c87abaddabb04c1ee178bcd648638ffa7428902
funlake <funlake@163.com>
2012-04-20 21:24:14 +0800

    delete docms

--- docms/modules/admin/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-class DOIndex extends DOController
-{
-	public function indexAction()
-	{
-		print_r(func_get_args());	
-	}
-}
-?>
--- docms/plugins/router/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array(
-	'setDefine' => true
-);
-?>
