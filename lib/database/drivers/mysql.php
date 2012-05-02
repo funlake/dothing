8adbbda16917eb21c762b623e7f54701676b8717
root <root@lake-pc.(none)>
2012-05-03 00:23:05 +0800

    add drivers

---
+++ lib/database/drivers/mysql.php
@@ -0,0 +1,16 @@
+<?php
+DOLoader::Import('lib.database.database');
+class DODatabaseMysql extends DODatabase
+{
+	public $driver = 'mysql';
+	public function SetDsn($host,$dbname)
+	{
+		$this->dsn = 'mysql:host='.$host.';dbname='.$dbname;
+	}
+	
+	public function IsError($errors)
+	{
+		return ($errors[1] > 999 && $errors[1] < 1200);
+	}
+}
+?>
