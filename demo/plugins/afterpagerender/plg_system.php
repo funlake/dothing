459278deeffb3bc9042fe5d275e99c735f4e4bf6
funlake <funlake@163.com>
2012-06-26 19:45:12 +0800

    added

---
+++ demo/plugins/afterpagerender/plg_system.php
@@ -0,0 +1,11 @@
+<?php
+class DOPlgSystemAfterpagerender extends DOPlugin
+{
+	public function Trigger( $response = null)
+	{
+		$mca 			= DORouter::GetMca();
+		$cache 			= DOFactory::GetCache(); 
+		return $cache->SetPageCache($mca,$response->GetBody());
+	}
+}
+?>
---
+++ demo/plugins/preparesql/plg_system.php
@@ -0,0 +1,7 @@
+<?php
+class DOPlgSystemPreparesql extends DOPlugin
+{
+	public function Trigger($database,$syntax,$params)
+	{//table separator handle probably here.
+	}
+}
