5699695fd1c7589167072dbf3dd2bb663c7b0726
funlake <funlake@163.com>
2012-09-13 21:12:00 +0800

    changed

--- demo/blocks/admin/leftmenu/leftmenu.php
+++ demo/blocks/admin/leftmenu/leftmenu.php
@@ -2,4 +2,32 @@
 !defined('DO_ACCESS') AND DIE("Go Away!");
 class DOBlocksAdminLeftmenu extends DOBlocksItem
 {
+	public function GetBackMenu()
+	{
+		$menu = array();
+		$menu['admin/system/setting'] = array(
+			array(
+				'title'		=> L('Global')
+			   ,'link'		=> '#'
+			   ,'class'		=> 'nav-header'
+			   ,'child'	=> array(
+			   		array(
+			   			'title' => L('Basic Info')
+			   		   ,'link'  => '#baseinfo'
+			   		)
+			   	   ,array(
+			   	   		'title' => L('System')
+			   	   	   ,'link'  => '#system'
+			   	   	)
+			   	   ,array(
+				   		'title'		=> L('Database')
+				   	   ,'link'		=> '#database'
+				   	   ,'class'     => 'active'
+		   			)
+			   	)
+			)
+
+		);
+		return $menu[DORouter::GetPageIndex()];
+	}
 }
