0aa9b9d1f9436b69781e91adf8a0eedbaa35f132
funlake <funlake@163.com>
2013-12-18 22:09:08 +0800

    add router

--- demo/router.php
+++ demo/router.php
@@ -4,3 +4,6 @@ DORouter::Map('/manage/user-:id','admin/user/edit');
 DORouter::Map('/manage/group','admin/user/group');
 DORouter::Map('/manage/group-:id','admin/user/editgroup');
 
+
+DORouter::Map('/forbidden','debug/index/nopermission');
+
---
+++ demo/views/modules/admin/user/editgroup.php
@@ -0,0 +1,72 @@
+<form action="http://localhost/dothing/demo/index.php/autocrud/Update/group" method="post" id="Afm" name="Afm" class="form-horizontal">
+	<div class="row well">
+		<div class="col-lg-8"><h4>Group / Update</h4></div>
+		<div class="col-lg-4 text-right">
+		  <button class="btn btn-success" id="submitForm">
+		  	<i class="glyphicon glyphicon-ok glyphicon-white"></i>
+		  	Apply		  </button>
+		  <button class="btn btn-danger" onclick="location.href=$('#__redirect').val();return false;">
+		  	<i class="glyphicon glyphicon-remove glyphicon-white"></i>
+		  	Cancel		  </button>
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="control-label col-lg-2" for="name">
+			Name		</label>
+		<div class="col-lg-4">
+			<input type="text" id="name" name="name" class="form-control" value="Customer" required/>
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="control-label col-lg-2" for="ordering">
+			Ordering		</label>
+		<div class="col-lg-4">
+			<input type="text" id="ordering" name="ordering" class="form-control" value="0" />
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="control-label col-lg-2" for="group_id">
+			Parent		</label>
+		<div class="col-lg-4">
+			<select id="group" data-placeholder="=====No Parent======" class="chzn-select form-control"  tabindex="2" name="pid" default="0" disable="7">
+				<option value="0"></option>
+								
+<?php $tree_2ce1cb285cfb355b697fcf2713002086=DOFactory::GetWidget("tree","default",array(DOFactory::GetModel(strtolower('Group'))->Select())) ?>
+<?php echo $tree_2ce1cb285cfb355b697fcf2713002086->Render("
+					<option value=\"{#id}\" parent=\"{#pid}\">[prefix]{#name}</option>
+				"); ?>
+
+			</select>
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="control-label col-lg-2" for="role_id">
+			Role		</label>
+		<div class="col-lg-4">
+			<select multiple data-placeholder="=======Choose roles======" class="chzn-select form-control"  tabindex="2" name="role_id[]" default="3">
+				<option value="0"></option>
+								
+<?php $tree_8cd712420b3a504a07dce68a6ed0b44b=DOFactory::GetWidget("tree","default",array(DOFactory::GetModel(strtolower('Role'))->Select())) ?>
+<?php echo $tree_8cd712420b3a504a07dce68a6ed0b44b->Render("
+					<option value=\"{#id}\" parent=\"{#pid}\">[prefix]{#name}</option>
+				"); ?>
+
+			</select>
+		</div>
+	</div>
+		<div class="form-group">
+		<label class="control-label col-lg-2" for="status">
+			Status		</label>
+		<div class="col-lg-4">
+			<label class="radio-inline">
+				<input id="status" name="state"  value="0" type="radio" /><label>No</label>
+			</label>
+			<label class="radio-inline">
+				<input id="status" name="state"  value="1" type="radio" checked/><label>Yes</label>
+			</label>
+		</div>
+	</div>
+	<input type="hidden" id="__redirect" name="__redirect" value="http://localhost/dothing/demo/index.php/manage/group"/>
+	<input type="hidden" id="group_id" name="id" value="7"/>
+	<input type="hidden" id="__token" name="__token" value="3376c86d25dd28ea295de750ee70c7c6"/>
+</form>
